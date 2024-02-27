615fbb417e09968a614a2738803e2564f86343a8
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-27 20:27:35 +0100

    'show' and 'remove' attributes added

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,5 +1,7 @@
 import { program } from 'commander'
 
+// coindex key -h
+
 program
   .command('set')
   .description('Set API key - Get at https://coinmarketcap.com/api/')
@@ -8,7 +10,27 @@ program
   })
 
   // -> commands:
-  //     - coindex key -h (--help)
+  //     - coindex key set
+
+program
+  .command('show')
+  .description('Show an API Key')
+  .action(() => {
+    console.log('Hello from set command')
+  })
+
+  // -> commands:
+  //     - coindex key show
+
+program
+  .command('remove')
+  .description('Remove an API Key')
+  .action(() => {
+    console.log('Hello from set command')
+  })
+
+  // -> commands:
+  //     - coindex key remove
 
 
 program.parse(process.argv)
