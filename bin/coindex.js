ce1b699e5ad9ba6a644b79b54ee1b43a12eeee8a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-27 20:00:17 +0100

    Basic commands (-h & -V)

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Hello from coindex!')
+import { program } from 'commander'
+
+program.version('1.0.0').parse(process.argv)
+// -> commands:
+//     - coindex -V (--version)
+//     - coindex -h (--help)
+
