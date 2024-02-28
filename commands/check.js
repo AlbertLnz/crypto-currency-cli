8a2b7dea723c607a264312f06305d0d45096fba4
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-28 11:56:02 +0100

    CryptoAPI class to manage the request w/ getPriceData() function

--- commands/check.js
+++ commands/check.js
@@ -1,5 +1,19 @@
+import { KeyManager } from "../lib/KeyManager.js"
+import { CryptoAPI } from "../lib/CryptoAPI.js"
+
 export const check = {
-  price(cmd) {
-    console.log(cmd.coin, '+', cmd.cur)
+  async price(cmd) {
+    try {
+      const keyManager = new KeyManager()
+      const key = keyManager.getKey()
+      
+      const api = new CryptoAPI(key)
+      const priceData = await api.getPriceData(cmd.coin, cmd.cur)
+
+      console.log(priceData)
+
+    } catch (error) {
+      console.error(error.message.bold.red)
+    }
   }
 }
